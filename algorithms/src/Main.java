fa9a91b0318d7e0d161328257be8bc59cc218db4
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2015-10-05 22:14:09 +0300

    Deikstri moved

--- algorithms/src/Main.java
+++ algorithms/src/Main.java
@@ -15,52 +15,6 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        Stack<String> ops = new Stack<String>();
-        Stack<Double> vals = new Stack<Double>();
-
-        while (scanner.hasNext()) {
-            String s = scanner.next();
-            switch (s) {
-                case ";":
-                    StdOut.println(vals.pop());
-                    System.exit(0);
-                case "(":
-                    break;
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                case "sqrt":
-                    ops.push(s);
-                    break;
-                case ")":
-                    String op = ops.pop();
-                    double v = vals.pop();
-                    switch (op) {
-                        case "+":
-                            v = vals.pop() + v;
-                            break;
-                        case "-":
-                            v = vals.pop() - v;
-                            break;
-                        case "*":
-                            v = vals.pop() * v;
-                            break;
-                        case "/":
-                            v = vals.pop() / v;
-                            break;
-                        case "sqrt":
-                            v = Math.sqrt(v);
-                            break;
-                    }
-                    vals.push(v);
-                    break;
-                default:
-                    vals.push(Double.parseDouble(s));
-                    break;
-            }
-        }
+        Deikstri.evaluate();
     }
 }
--- algorithms/src/deikstri/Deikstri.java
+++ algorithms/src/deikstri/Deikstri.java
@@ -1,8 +1,8 @@
 package deikstri;
 
-import utils.StdIn;
 import utils.StdOut;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
@@ -10,30 +10,53 @@ import java.util.Stack;
  */
 public class Deikstri {
 
-    public static void evaluate(String[] args) {
+    public static void evaluate() {
+        Scanner scanner = new Scanner(System.in);
+
         Stack<String> ops = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
 
-        while (!StdIn.isEmpty()) {
-            String s = StdIn.readString();
-            if (s.equals("(")) ;
-            else if (s.equals("+")) ops.push(s);
-            else if (s.equals("-")) ops.push(s);
-            else if (s.equals("*")) ops.push(s);
-            else if (s.equals("/")) ops.push(s);
-            else if (s.equals("sqrt")) ops.push(s);
-            else if (s.equals(")")) {
-                String op = ops.pop();
-                double v = vals.pop();
-                if (op.equals("+")) v = vals.pop() + v;
-                else if (op.equals("-")) v = vals.pop() - v;
-                else if (op.equals("*")) v = vals.pop() * v;
-                else if (op.equals("/")) v = vals.pop() / v;
-                else if (op.equals("sqrt")) v = Math.sqrt(v);
-                vals.push(v);
-            } else vals.push(Double.parseDouble(s));
+        while (scanner.hasNext()) {
+            String s = scanner.next();
+            switch (s) {
+                case ";":
+                    StdOut.println(vals.pop());
+                    System.exit(0);
+                case "(":
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "sqrt":
+                    ops.push(s);
+                    break;
+                case ")":
+                    String op = ops.pop();
+                    double v = vals.pop();
+                    switch (op) {
+                        case "+":
+                            v = vals.pop() + v;
+                            break;
+                        case "-":
+                            v = vals.pop() - v;
+                            break;
+                        case "*":
+                            v = vals.pop() * v;
+                            break;
+                        case "/":
+                            v = vals.pop() / v;
+                            break;
+                        case "sqrt":
+                            v = Math.sqrt(v);
+                            break;
+                    }
+                    vals.push(v);
+                    break;
+                default:
+                    vals.push(Double.parseDouble(s));
+                    break;
+            }
         }
-        StdOut.println(vals.pop());
-
     }
 }
