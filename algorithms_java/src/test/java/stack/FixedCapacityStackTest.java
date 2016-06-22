c621b353445fc747e533a3a358ac0699aef01c53
Kolodgeeva <Mariana.Kolodgeeva@vistajet.com>
2016-06-22 13:48:07 +0300

    Add FixedCapacityStack

--- algorithms_java/src/test/java/stack/FixedCapacityStackTest.java
+++ algorithms_java/src/test/java/stack/FixedCapacityStackTest.java
@@ -1,29 +1,44 @@
 package stack;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by User on 6/1/2016.
  */
 public class FixedCapacityStackTest {
 
-    @Test
-    public void test() {
-        /*int max = Integer.parseInt(args[0]);
-        FixedCapacityStack<String> stack = new FixedCapacityStack<String>(max);
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) stack.push(item);
-            else if (stack.isEmpty())  StdOut.println("BAD INPUT");
-            else                       StdOut.print(stack.pop() + " ");
-        }
-        StdOut.println();
+    private BufferedReader in = null;
 
-        // print what's left on the stack
-        StdOut.print("Left on stack: ");
-        for (String s : stack) {
-            StdOut.print(s + " ");
+    @Before
+    public void setup() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        in = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream("FixedCapacityStackData.txt")));
+    }
+
+    @After
+    public void teardown() throws IOException {
+        if (in != null) {
+            in.close();
         }
-        StdOut.println();*/
+        in = null;
+    }
+
+    @Test
+    public void test() throws IOException {
+        List<String> strings = in.lines().collect(Collectors.toList());
+        FixedCapacityStack<String> stack = new FixedCapacityStack<>(strings.size());
+        strings.forEach(stack::push);
+        System.out.println("Read stack: ");
+        for (String s : stack) {
+            System.out.println(s + " ");
+        }
     }
 }
